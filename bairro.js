16d89c23cb5ee8eb8754b180f36bfe160b9d208b
TaldoJao1 <joaopedrosilveriosilva42@gmail.com>
2024-06-07 09:50:03 -0300

    Add files via upload

---
+++ bairro.js
@@ -0,0 +1,8 @@
+var bairros = document.getElementsByName('bairros').values;
+var vilaFor = document.getElementsByName('vila formosa').values;
+var vilaCar = document.getElementsByName('vila carrao').values;
+
+
+if(bairros == vilaCar){
+    console.log("cu")
+}
